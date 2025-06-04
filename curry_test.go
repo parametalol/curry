c953755d2bd033602be239b34e4ad76b62a9e555
Tomasz Janiszewski <tomek@redhat.com>
2025-06-04 13:27:42 +0200

    remove dependecies
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

--- curry_test.go
+++ curry_test.go
@@ -1,10 +1,8 @@
 package curry
 
 import (
+	"errors"
 	"testing"
-
-	"github.com/pkg/errors"
-	"github.com/stretchr/testify/assert"
 )
 
 func join2(a, b string) string {
@@ -32,53 +30,53 @@ func join4e(a, b, c, d string) (string, error) {
 }
 
 func TestDrop(t *testing.T) {
-	assert.Equal(t, 1, DropLast2(1, 2))
-	assert.Equal(t, 1, DropLast2(DropLast3(1, 2, 3)))
-	assert.Equal(t, 1, DropLast2(DropLast3(DropLast4(1, 2, 3, 4))))
+	equal(t, 1, DropLast2(1, 2))
+	equal(t, 1, DropLast2(DropLast3(1, 2, 3)))
+	equal(t, 1, DropLast2(DropLast3(DropLast4(1, 2, 3, 4))))
 
-	assert.Equal(t, 2, DropFirst2(1, 2))
-	assert.Equal(t, 3, DropFirst2(DropFirst3(1, 2, 3)))
-	assert.Equal(t, 4, DropFirst2(DropFirst3(DropFirst4(1, 2, 3, 4))))
+	equal(t, 2, DropFirst2(1, 2))
+	equal(t, 3, DropFirst2(DropFirst3(1, 2, 3)))
+	equal(t, 4, DropFirst2(DropFirst3(DropFirst4(1, 2, 3, 4))))
 }
 
 func TestCurry(t *testing.T) {
-	assert.Equal(t, "ab", Curry2(join2)("a")("b"))
-	assert.Equal(t, "abc", Curry3(join3)("a")("b")("c"))
-	assert.Equal(t, "abcd", Curry4(join4)("a")("b")("c")("d"))
+	equal(t, "ab", Curry2(join2)("a")("b"))
+	equal(t, "abc", Curry3(join3)("a")("b")("c"))
+	equal(t, "abcd", Curry4(join4)("a")("b")("c")("d"))
 
-	assert.Equal(t, "ab", DropLast2(Curry2e(join2e)("a")("b")))
-	assert.Equal(t, "abc", DropLast2(Curry3e(join3e)("a")("b")("c")))
-	assert.Equal(t, "abcd", DropLast2(Curry4e(join4e)("a")("b")("c")("d")))
+	equal(t, "ab", DropLast2(Curry2e(join2e)("a")("b")))
+	equal(t, "abc", DropLast2(Curry3e(join3e)("a")("b")("c")))
+	equal(t, "abcd", DropLast2(Curry4e(join4e)("a")("b")("c")("d")))
 }
 
 func TestUnCurry(t *testing.T) {
 	curriedJoin := Curry4(join4)
-	assert.Equal(t, "abcd", UnCurry4(curriedJoin)("a", "b", "c", "d"))
-	assert.Equal(t, "abcd", UnCurry3(curriedJoin)("a", "b", "c")("d"))
-	assert.Equal(t, "abcd", UnCurry2(curriedJoin)("a", "b")("c")("d"))
+	equal(t, "abcd", UnCurry4(curriedJoin)("a", "b", "c", "d"))
+	equal(t, "abcd", UnCurry3(curriedJoin)("a", "b", "c")("d"))
+	equal(t, "abcd", UnCurry2(curriedJoin)("a", "b")("c")("d"))
 
 	curriedJoinE := Curry4e(join4e)
-	assert.Equal(t, "abcd", DropLast2(UnCurry4e(curriedJoinE)("a", "b", "c", "d")))
-	assert.Equal(t, "abcd", DropLast2(UnCurry3(curriedJoinE)("a", "b", "c")("d")))
-	assert.Equal(t, "abcd", DropLast2(UnCurry2(curriedJoinE)("a", "b")("c")("d")))
+	equal(t, "abcd", DropLast2(UnCurry4e(curriedJoinE)("a", "b", "c", "d")))
+	equal(t, "abcd", DropLast2(UnCurry3(curriedJoinE)("a", "b", "c")("d")))
+	equal(t, "abcd", DropLast2(UnCurry2(curriedJoinE)("a", "b")("c")("d")))
 }
 
 func TestBind(t *testing.T) {
-	assert.Equal(t, "ab", Bind1st2(join2, "a")("b"))
-	assert.Equal(t, "abc", Bind1st3(join3, "a")("b", "c"))
-	assert.Equal(t, "abcd", Bind1st4(join4, "a")("b", "c", "d"))
+	equal(t, "ab", Bind1st2(join2, "a")("b"))
+	equal(t, "abc", Bind1st3(join3, "a")("b", "c"))
+	equal(t, "abcd", Bind1st4(join4, "a")("b", "c", "d"))
 
-	assert.Equal(t, "ab", DropLast2(Bind1st2e(join2e, "a")("b")))
-	assert.Equal(t, "abc", DropLast2(Bind1st3e(join3e, "a")("b", "c")))
-	assert.Equal(t, "abcd", DropLast2(Bind1st4e(join4e, "a")("b", "c", "d")))
+	equal(t, "ab", DropLast2(Bind1st2e(join2e, "a")("b")))
+	equal(t, "abc", DropLast2(Bind1st3e(join3e, "a")("b", "c")))
+	equal(t, "abcd", DropLast2(Bind1st4e(join4e, "a")("b", "c", "d")))
 
-	assert.Equal(t, "ba", BindLast2(join2, "a")("b"))
-	assert.Equal(t, "bca", BindLast3(join3, "a")("b", "c"))
-	assert.Equal(t, "bcda", BindLast4(join4, "a")("b", "c", "d"))
+	equal(t, "ba", BindLast2(join2, "a")("b"))
+	equal(t, "bca", BindLast3(join3, "a")("b", "c"))
+	equal(t, "bcda", BindLast4(join4, "a")("b", "c", "d"))
 
-	assert.Equal(t, "ba", DropLast2(BindLast2e(join2e, "a")("b")))
-	assert.Equal(t, "bca", DropLast2(BindLast3e(join3e, "a")("b", "c")))
-	assert.Equal(t, "bcda", DropLast2(BindLast4e(join4e, "a")("b", "c", "d")))
+	equal(t, "ba", DropLast2(BindLast2e(join2e, "a")("b")))
+	equal(t, "bca", DropLast2(BindLast3e(join3e, "a")("b", "c")))
+	equal(t, "bcda", DropLast2(BindLast4e(join4e, "a")("b", "c", "d")))
 }
 
 func TestCombinations(t *testing.T) {
@@ -86,12 +84,12 @@ func TestCombinations(t *testing.T) {
 
 	t.Run("bind 2nd of three", func(t *testing.T) {
 		boundSecondOfThree := UnCurry2(BindLast2(bindFirstTwoOf3, "b"))
-		assert.Equal(t, "abc", boundSecondOfThree("a", "c"))
+		equal(t, "abc", boundSecondOfThree("a", "c"))
 	})
 
 	t.Run("bind first two of three", func(t *testing.T) {
 		boundFirstTwo := bindFirstTwoOf3("a", "b")
-		assert.Equal(t, "abc", boundFirstTwo("c"))
+		equal(t, "abc", boundFirstTwo("c"))
 	})
 
 }
@@ -101,8 +99,8 @@ func TestDifferentTypes(t *testing.T) {
 	f1 := func(int, string) bool {
 		return true
 	}
-	var b bool = Curry2(f1)(42)("abc")
-	assert.True(t, b)
+	var b = Curry2(f1)(42)("abc")
+	equal(t, true, b)
 
 	testErr := errors.New("err")
 	f1e := func(int, string) (bool, error) {
@@ -110,18 +108,27 @@ func TestDifferentTypes(t *testing.T) {
 	}
 	var err error
 	b, err = Curry2e(f1e)(42)("abc")
-	assert.True(t, b)
-	assert.ErrorIs(t, err, testErr)
+	equal(t, true, b)
+	equal(t, testErr, err)
 
 	b = UnCurry2(Curry2(f1))(42, "abc")
-	assert.True(t, b)
+	equal(t, true, b)
 
 	b, err = UnCurry2e(Curry2e(f1e))(42, "abc")
-	assert.True(t, b)
-	assert.ErrorIs(t, err, testErr)
+	equal(t, true, b)
+	equal(t, testErr, err)
 
 	err = DropFirst2(f1e(42, "abc"))
-	assert.ErrorIs(t, err, testErr)
+	equal(t, testErr, err)
 	b = DropLast2(f1e(42, "abc"))
-	assert.True(t, b)
+	equal(t, true, b)
+}
+
+func equal[C comparable](t *testing.T, expected, actual C) {
+	t.Helper()
+	if expected == actual {
+		return
+	}
+	t.Errorf("expected = %v, got = %v", expected, actual)
+	t.Fail()
 }
