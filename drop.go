e3f10965b5fda3a9bfaef0f380b6624f42052ca3
Michaël Petrov <smartptr@gmail.com>
2025-06-08 21:45:22 +0200

    DropOne

--- drop.go
+++ drop.go
@@ -2,28 +2,31 @@ package curry
 
 // region DropFirst
 
-func DropFirstOfTwo[A, B any](_ A, b B) B {
+// DropOne
+func DropOne(any) {}
+
+func DropFirstOfTwo[B any](_ any, b B) B {
 	return b
 }
 
-func DropFirstOfThree[A, B, C any](_ A, b B, c C) (B, C) {
+func DropFirstOfThree[B, C any](_ any, b B, c C) (B, C) {
 	return b, c
 }
 
-func DropFirstOfFour[A, B, C, D any](_ A, b B, c C, d D) (B, C, D) {
+func DropFirstOfFour[B, C, D any](_ any, b B, c C, d D) (B, C, D) {
 	return b, c, d
 }
 
 // region DropLast
 
-func DropLastOfTwo[A, B any](a A, _ B) A {
+func DropLastOfTwo[A any](a A, _ any) A {
 	return a
 }
 
-func DropLastOfThree[A, B, C any](a A, b B, _ C) (A, B) {
+func DropLastOfThree[A, B any](a A, b B, _ any) (A, B) {
 	return a, b
 }
 
-func DropLastOfFour[A, B, C, D any](a A, b B, c C, _ D) (A, B, C) {
+func DropLastOfFour[A, B, C any](a A, b B, c C, _ any) (A, B, C) {
 	return a, b, c
 }
